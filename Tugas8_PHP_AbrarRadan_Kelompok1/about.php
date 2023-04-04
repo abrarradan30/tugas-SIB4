1615716bd94b9bdf2931c44a84004315ef139b8f
abrarradan30 <abradan84@gmail.com>
2023-04-04 22:57:25 +0700

    menambahkan tugas 8 php

---
+++ Tugas8_PHP_AbrarRadan_Kelompok1/about.php
@@ -0,0 +1,23 @@
+<?php
+include_once 'top.php';
+include_once 'menu.php';
+?>
+
+<div class="About-us">
+    <h1> About Us </h1>
+	<br>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisci elit,</p>
+	<hr>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. 
+        Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+   <h3> follow us on </h3>
+   <a href="#"> Instagram </a> 
+   <a href="#"> Twitter </a> 
+   <a href="#"> Youtube </a> 
+  </div>
+
+<?php
+include_once 'bottom.php';
+?>
---
+++ Tugas8_PHP_AbrarRadan_Kelompok1/contact.php
@@ -0,0 +1,21 @@
+<?php
+include_once 'top.php';
+include_once 'menu.php';
+?>
+
+<body>
+    <h2>Tentang Kami</h2>
+    <br>
+        <form action="">
+            <h2>Contact Us</h2>
+            <input type="text" placeholder="Nama">
+            <input type="email" placeholder="Alamat Email">
+            <input type="text" placeholder="Telepone">
+            <button>Kirim</button>
+        </form>
+        <br>
+</body>
+
+<?php
+include_once 'bottom.php';
+?>
