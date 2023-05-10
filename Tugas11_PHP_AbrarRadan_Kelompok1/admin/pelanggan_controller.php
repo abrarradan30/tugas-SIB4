8655b614b0cf3332a3daa76f1dcc8afc527eebcf
abrarradan30 <abradan84@gmail.com>
2023-05-10 20:17:22 +0700

    menambahkan tugas 11 php

---
+++ Tugas11_PHP_AbrarRadan_Kelompok1/admin/pelanggan_controller.php
@@ -0,0 +1,38 @@
+<?php
+include_once 'koneksi.php';
+include_once 'models/pelanggan.php';
+
+// 1. menangkap request form
+$kode = $_POST['kode'];
+$nama = $_POST['nama_pelanggan'];
+$jk = $_POST['jk'];
+$tmp_lahir = $_POST['tmp_lahir'];
+$tgl_lahir = $_POST['tgl_lahir'];
+$email = $_POST['email'];
+$kartu_id = $_POST['kartu_id'];
+
+// menangkap form diatas dijadikan array
+$data = [
+    $kode,
+    $nama_pelanggan,
+    $jk,
+    $tmp_lahir,
+    $tgl_lahir,
+    $email,
+    $kartu_id
+];
+
+$model = new Pelanggan();
+$tombol = $_REQUEST['proses'];
+switch($tombol){
+    case 'simpan':$model->simpan($data); break;
+    case 'ubah':
+        $data[] = $_POST[idx]; $model->ubah($data); break;
+    case 'hapus':
+        unset($data); $model->hapus($_POST['idx']); break;
+    default:
+    header('Location:index.php?url=pelanggan');
+    break;
+}
+header('Location:index.php?url=pelanggan');
+?>
